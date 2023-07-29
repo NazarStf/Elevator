853496d3cf35068232e11197cbf9ec1e6d0411de
NazarStf <hworker98@gmail.com>
2023-07-29 13:23:31 +0300

    Initial Commit

---
+++ Elevator/ElevatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElevatorApp.swift
+//  Elevator
+//
+//  Created by NazarStf on 29.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ElevatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
